2095ffcb8379355787c090e966035c122ed38db6
Marth <tuantran00541@gmail.com>
2026-09-20 06:22:53 +0700

    test(bonsai2): make PTQ1 2-bit timer C11 portable

--- qwen38/test_bonsai2_ptq1_2bit.c
+++ qwen38/test_bonsai2_ptq1_2bit.c
@@ -38,7 +38,7 @@ static uint32_t f32_bits(float x) {
 
 static double now_seconds(void) {
     struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
+    if (timespec_get(&ts, TIME_UTC) != TIME_UTC) return 0.0;
     return (double)ts.tv_sec + (double)ts.tv_nsec * 1e-9;
 }
 
